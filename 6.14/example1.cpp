5fb3ac58344de2925e92219c37d89290d5a782a9
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-26 00:45:37 +0900

    constexpr-and-consteval-functions

---
+++ 6.14/example1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+constexpr int greater(int x, int y) // now a constexpr function
+{
+  return (x > y ? x : y);
+}
+
+int main() {
+  constexpr int x{5};
+  constexpr int y{6};
+
+  // We'll explain why we use variable g here later in the lesson
+  constexpr int g{greater(x, y)}; // will be evaluated at compile-time
+
+  std::cout << g << " is greater!\n";
+
+  return 0;
+}
