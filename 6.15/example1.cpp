bc08e2bef044787293a5e1a8e1c6a4eb5a7c655d
0xsuk <97814789+0xsuk@users.noreply.github.com>
2022-09-26 16:46:18 +0900

    unnamed-and-inline-namespace

---
+++ 6.15/example1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace // unnamed namespace
+{
+void doSomething() { // can only be accessed in this file
+  std::cout << "v1\n";
+}
+} // namespace
+
+int main() {
+  doSomething(); // we can call doSomething() without a namespace prefix
+
+  return 0;
+}
